c41481d4f8871feba466956a1901056141e12886
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-22 11:19:23 +0300

    cleaning up and setting up initial tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,14 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+describe("App Component", () => {
+  it("Exist", () => {
+    // assert
+    expect(AppComponent).toBeDefined();
   });
-
-  it(`should have as title 'pj-docs-site'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('pj-docs-site');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to pj-docs-site!');
+  it("Can be built", () => {
+    //act
+    const component = new AppComponent();
+    // assert
+    expect(component instanceof AppComponent).toBe(true);
   });
 });
