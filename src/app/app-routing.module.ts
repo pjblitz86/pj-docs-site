8549cbec1e1198056eeec46c944a8e62912fc3ff
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-30 16:41:06 +0300

    setup lazy loading for HomeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { HomeComponent } from "./views/home/home.component";
 
-const routes: Routes = [{ path: "", component: HomeComponent }];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () =>
+      import("./views/home/home.module").then(module => module.HomeModule)
+  }
+];
 
 @NgModule({
   declarations: [],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { SideNavComponent } from "./components/side-nav/side-nav.component";
-import { HomeComponent } from "./views/home/home.component";
 import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FooterComponent,
-    SideNavComponent,
-    HomeComponent
-  ],
+  declarations: [AppComponent, FooterComponent, SideNavComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
