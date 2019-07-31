10652435b69d5cfeb4ad2c19199d437289b0ff3f
Paulius_Juknis <paulius.juknis@gmail.com>
2019-07-31 09:14:36 +0300

    adding manifest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from "./app.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { SideNavComponent } from "./components/side-nav/side-nav.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, FooterComponent, SideNavComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
