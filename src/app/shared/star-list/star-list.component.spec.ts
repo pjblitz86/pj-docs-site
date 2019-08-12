26c250ae0bfd91f6aadfa3e0458e2027b2980fc6
Paulius_Juknis <paulius.juknis@gmail.com>
2019-08-12 13:52:33 +0300

    tests for starlist component star calculation logic

--- src/app/shared/star-list/star-list.component.spec.ts
+++ src/app/shared/star-list/star-list.component.spec.ts
@@ -1,6 +1,6 @@
 import { StarListComponent } from "./star-list.component";
 
-describe("Book Component", () => {
+describe("StarList Component", () => {
   it("Exist", () => {
     // assert
     expect(StarListComponent).toBeDefined();
@@ -10,5 +10,68 @@ describe("Book Component", () => {
     const component = new StarListComponent();
     // assert
     expect(component instanceof StarListComponent).toBe(true);
+  });
+
+  describe("General", () => {
+    let component: StarListComponent;
+
+    beforeEach(() => {
+      component = new StarListComponent();
+    });
+
+    describe("Defaults", () => {
+      it("rating has 5 stars when no rating", () => {
+        //assert
+        expect(component.rating).toBe(5);
+      });
+    });
+
+    describe("General", () => {
+      it("Gets full star rating", () => {
+        //arrange
+        component.rating = 4.75;
+
+        //act
+        const result = component.fullStars;
+
+        // assert
+        expect(result.length).toBe(4);
+      });
+
+      describe("Half star rating", () => {
+        it("Shows half star rating when not highest rating", () => {
+          //arrange
+          component.rating = 4.75;
+
+          //act
+          const result = component.hasHalfStar;
+
+          // assert
+          expect(result).toBe(true);
+        });
+
+        it("Doesnt show half star rating when highest rating", () => {
+          //arrange
+          component.rating = 5;
+
+          //act
+          const result = component.hasHalfStar;
+
+          // assert
+          expect(result).toBe(false);
+        });
+      });
+
+      it("Gets empty star rating", () => {
+        //arrange
+        component.rating = 2.75;
+
+        //act
+        const result = component.emptyStars;
+
+        // assert
+        expect(result.length).toBe(2);
+      });
+    });
   });
 });
